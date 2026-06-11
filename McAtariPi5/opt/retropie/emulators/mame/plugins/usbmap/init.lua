8450f893ca6071eaed7fba14fe53cc93b82da7b7
Dan Caputi <dcaputi1@gmail.com>
2026-06-11 16:54:36 -0400

    usbmap v1.3

--- McAtariPi5/opt/retropie/emulators/mame/plugins/usbmap/init.lua
+++ McAtariPi5/opt/retropie/emulators/mame/plugins/usbmap/init.lua
@@ -10,7 +10,7 @@
 -- unit is treated as P1.
 -----------------------------------------------------------
 
-local VERSION = "1.2.0"
+local VERSION = "1.3.0"
 
 local exports = {
     name        = "usbmap",
